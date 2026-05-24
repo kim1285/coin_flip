6092b352d1a8e25618f8d9f05c2471f405887017
Kangsan Kim <rlarkdtks8713@gmail.com>
2026-05-24 17:54:32 +0900

    feat: [FC-0005] created means to get db session

---
+++ db.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+from typing import Annotated, AsyncGenerator
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, AsyncEngine
+
+
+@lru_cache
+def get_engine(db_url) -> AsyncEngine:
+    engine = create_async_engine(url=db_url, echo=True)
+    return engine
+
+
+@lru_cache
+def get_async_session_maker(db_url) -> async_sessionmaker[AsyncSession]:
+    engine = get_engine(db_url)
+    return async_sessionmaker(engine)
+
+
+async def get_session(async_session_maker: async_sessionmaker[AsyncSession]) -> AsyncGenerator[AsyncSession]:
+    async with async_session_maker() as session:
+        yield session
+
+
+DBSessionDep = Annotated[AsyncSession, Depends(get_session)]
